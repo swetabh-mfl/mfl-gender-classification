062c1497d79d0f48dee1001c8a3dc623d691ae11
swetabh-mfl <swetabh.shubham@moonfroglabs.com>
2024-07-16 17:50:09 +0530

    model_training_file

--- scripts/model_training.py
+++ scripts/model_training.py
@@ -10,7 +10,12 @@ from model_building import build_model
 
 def train_model(train_dir, validation_dir, model_save_path, epochs=10, batch_size=32, num_classes=2):
     train_generator, validation_generator = prepare_data(train_dir, validation_dir)
-    model = build_model(num_classes=num_classes)  # Pass num_classes argument here
+    model = build_model(num_classes=num_classes)
+
+    # Compile the model
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
 
     checkpoint = ModelCheckpoint(
         model_save_path,
@@ -39,9 +44,9 @@ def train_model(train_dir, validation_dir, model_save_path, epochs=10, batch_siz
 if __name__ == "__main__":
     train_dir = 'data/train'
     validation_dir = 'data/validation'
-    model_save_path = 'models/gender_classification_model.h5'  # Corrected path
+    model_save_path = 'models/gender_classification_model.h5'
     epochs = 10
     batch_size = 32
-    num_classes = 2  # Replace with actual number of classes in your dataset
+    num_classes = 2
 
     train_model(train_dir, validation_dir, model_save_path, epochs, batch_size, num_classes)
